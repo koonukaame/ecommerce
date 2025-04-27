cb1890ea8e22223df688467baee89cccbdaefd5c
unknown <tarpel.gta@gmail.com>
2025-04-27 23:11:58 +0300

    chore: 3 - Configure eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,105 @@
+import js from "@eslint/js";
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import unicorn from 'eslint-plugin-unicorn';
+import perfectionist from 'eslint-plugin-perfectionist';
+import globals from "globals";
+
+export default [
+  {
+    ...js.configs.recommended,
+    settings: {
+      noInlineConfig: true,
+      reportUnusedDisableDirectives: true,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+    },
+    rules: {
+      ...ts.configs.recommended.rules,
+      "no-console": 0,
+      '@typescript-eslint/consistent-type-assertions': [
+        'error',
+        { assertionStyle: 'never' },
+      ],
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: true,
+          allowHigherOrderFunctions: true,
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        { accessibility: 'explicit', overrides: { constructors: 'off' } },
+      ],
+      '@typescript-eslint/member-ordering': 'warn'
+    },
+  },
+
+  {
+    plugins: {
+      unicorn,
+    },
+    rules: {
+      ...unicorn.configs.recommended.rules,
+      'unicorn/no-array-callback-reference': 'off',
+      'unicorn/no-array-for-each': 'off',
+      'unicorn/no-array-reduce': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/number-literal-case': 'off',
+      'unicorn/numeric-separators-style': 'off',
+      'unicorn/prevent-abbreviations': [
+        'error',
+        {
+          allowList: {
+            acc: true,
+            env: true,
+            i: true,
+            j: true,
+            props: true,
+            Props: true,
+          },
+        },
+      ],
+    },
+  },
+
+  {
+    ...perfectionist.configs['recommended-natural'],
+  },
+
+  {
+    ignores: [
+      "eslint.config.mjs",
+      "/.git",
+      "/.vscode",
+      "**/node_modules",
+      "/dist",
+      "/public",
+    ],
+  },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import './style.css';
+
+const object: {[key: string]: string} = {
+  a: 'bbbb',
+  bb: 'aaa',
+  ccc: 'ccccccc',
+  dddd: 'ddddd',
+  x: 'x',
+
+}
+
+console.log(object);
