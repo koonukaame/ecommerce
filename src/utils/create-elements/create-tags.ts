b98f9e9a1a7499350a4cd4d68480afdcf77a1403
koonukaame <marialezhebokova@yandex.ru>
2025-05-05 01:18:42 +0300

    feat: 37 - implement element factory creator and tag factories

---
+++ src/utils/create-elements/create-tags.ts
@@ -0,0 +1,15 @@
+import type { Options } from './types';
+
+import { createElement } from './create-elements';
+
+type ElementOptions = Omit<Options<keyof HTMLElementTagNameMap>, 'tag'>;
+
+function createElementFactory<K extends keyof HTMLElementTagNameMap>(
+  tag: K
+): (options: ElementOptions) => HTMLElementTagNameMap[K] {
+  return (options) => {
+    return createElement({ tag, ...options });
+  };
+}
+
+export const createDiv = createElementFactory('div');
