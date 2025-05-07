9649c03b1dd6e95999d65b773b5c608ceb037819
koonukaame <marialezhebokova@yandex.ru>
2025-05-07 15:56:49 +0300

    feat: 10 - add interactivity for button and link

--- src/pages/login/login-page.ts
+++ src/pages/login/login-page.ts
@@ -3,36 +3,32 @@ import { buttonStyles } from "../../shared/styles";
 import { createA, createButton, createDiv, createForm, createH1, createInput, createLabel, createMain } from "../../utils/create-elements/create-tags";
 
 export function LoginPage(): HTMLElement {
-  const container = createMain({ classes: ['flex', 'justify-center', 'items-center', 'min-h-screen', 'p-3'], parent: document.body})
+  const container = createMain({ classes: ['flex', 'justify-center', 'items-center', 'min-h-screen', 'p-3'], parent: document.body});
   const wrapper = createDiv({ classes: ['flex', 'flex-col', 'max-w-[600px]', 'w-full', 'items-center', 'gap-[30px]', 'p-3', 'border', 'border-[#252525]/50'], parent: container });
 
   createH1({classes: ['text-[20px]'], parent: wrapper, text: 'Registration'});
 
-  const inputsContainer = createForm({ classes: ['flex', 'flex-col', 'gap-[20px]', 'w-full', 'items-center'], parent: wrapper })
+  const inputsContainer = createForm({ classes: ['flex', 'flex-col', 'gap-[20px]', 'w-full', 'items-center'], parent: wrapper });
 
-  createInputField('email', 'Enter your email*', inputsContainer, (value) => { console.log(`email: ${value}`)})
+  createInputField('email', 'Enter your email*', inputsContainer, (value) => { console.log(`email: ${value}`)});
 
-  createInputField('password', 'Enter your password*', inputsContainer, (value) => { console.log(`password: ${value}`)})
+  createInputField('password', 'Enter your password*', inputsContainer, (value) => { console.log(`password: ${value}`)});
 
-  const passwordVisibilityWrapper = createDiv({ classes: ['w-full', 'max-w-[400px]', 'flex', 'justify-end', 'gap-1'], parent: inputsContainer})
+  const passwordVisibilityWrapper = createDiv({ classes: ['w-full', 'max-w-[400px]', 'flex', 'justify-end', 'gap-1'], parent: inputsContainer});
 
-  createInput({ 
-    attributes: {id: 'password', type: 'checkbox' }, 
-    parent: passwordVisibilityWrapper,
-  });
+  createInput({ attributes: {id: 'password', type: 'checkbox' }, parent: passwordVisibilityWrapper, });
 
-  createLabel({ attributes: { for: 'password' }, parent: passwordVisibilityWrapper, text: 'Show password' })
+  createLabel({ attributes: { for: 'password' }, parent: passwordVisibilityWrapper, text: 'Show password' });
 
   createButton({
     attributes: { type: 'button' }, //! Was added to prevent redirecting; consider removing it when validation will be done
-    classes: [...buttonStyles.general, 'cursor-pointer'],
+    classes: [...buttonStyles.general, ...buttonStyles.generalHover, ...buttonStyles.generalFocus],
     events: { click: () => console.log('clicked')},
     parent: inputsContainer,
     text: 'Login',
-
   })
 
-  createA({ classes: ['cursor-pointer'], events: { click: () => console.log('clicked')}, parent: wrapper, text: "Don't have an account? Register" })
+  createA({ classes: ['cursor-pointer', 'duration-300', 'hover:underline'], events: { click: () => console.log('clicked')}, parent: wrapper, text: "Don't have an account? Register" });
 
   return container;
 }
--- src/shared/styles.ts
+++ src/shared/styles.ts
@@ -3,5 +3,7 @@ export const inputStyles = {
 }
 
 export const buttonStyles = {
-  general: ['w-full', 'max-w-[400px]', 'h-[50px]', 'bg-[#E0BEA2]', 'text-white/100', 'p-2' ],
+  general: ['w-full', 'max-w-[400px]', 'h-[50px]', 'bg-[#E0BEA2]', 'text-white/100', 'p-2', 'duration-500', 'cursor-pointer'],
+  generalFocus: ['transition-shadow', 'active:shadow-[inset_0_3px_7px_0_rgba(37,37,37,0.15),0_10px_10px_0_rgba(37,37,37,0.15)]'],
+  generalHover: ['transition-shadow', 'hover:shadow-[0_10px_10px_0_rgba(37,37,37,0.15)]', 'duration-500']
 }
