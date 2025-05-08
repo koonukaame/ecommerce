41a327801bd6d897f1449a5f02d23a74826824c6
Alex <strelchenko.aleksandr@yandex.ru>
2025-05-08 07:49:39 +0300

    feat: create getAnonymousTokn function

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
+import { getAnonymousToken } from './app/ecommerce/get-anonymous-token';
 import './style.css';
+
+// test
+// delete the lines below
+try {
+  const result = await getAnonymousToken();
+  console.log(result);
+} catch (error) {
+  console.log(error);
+}
