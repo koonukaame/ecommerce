e626bf86090b3de715e723676dd2634c982dca55
unknown <tarpel.gta@gmail.com>
2025-05-08 09:10:08 +0300

    refactor: 25-(toggle-classes-on-direct) replace methods add/remove on toggle class

--- src/helpers/toggle-classes-on-redirect.ts
+++ src/helpers/toggle-classes-on-redirect.ts
@@ -1,21 +1,6 @@
 export const toggleClassesOnRedirect = function (isLogined: boolean = true, page: string = 'main'): void {
   const bodyElement = document.body;
 
-  if (isLogined) {
-    bodyElement.classList.add('is-logined');
-  } else {
-    bodyElement.classList.remove('is-logined');
-  }
-
-  if (page === 'main') {
-    bodyElement.classList.add('mainpage');
-    if (!bodyElement.classList.contains('background')) {
-      bodyElement.classList.add('background');
-    }
-  } else {
-    bodyElement.classList.remove('mainpage');
-    if (bodyElement.classList.contains('background')) {
-      bodyElement.classList.remove('background');
-    }
-  }
+  bodyElement.classList.toggle('is-logined', isLogined);
+  bodyElement.classList.toggle('mainpage', page === 'main');
 }
