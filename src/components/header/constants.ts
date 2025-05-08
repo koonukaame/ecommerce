81c917e0926b7b147c6472d870258be4f863f7dd
unknown <tarpel.gta@gmail.com>
2025-05-08 03:17:46 +0300

    refactor: 25-move constants from layout.ts into constants.ts file

--- src/components/header/constants.ts
+++ src/components/header/constants.ts
@@ -1,4 +1,10 @@
-export const HEADER_CLASSES: {[key: string]: string[]} = {
+import { createButtonWithSVG } from '../../utils/create-elements/create-svg-buttom';
+import { 
+  createButton,
+  createH1, 
+ } from '../../utils/create-elements/create-tags';
+
+const HEADER_CLASSES: {[key: string]: string[]} = {
   header: ["h-16", "grid", "gap-0", "px-2", "md:px-8", "grid-cols-3", "grid-flow-col", "md:grid-cols-[max-content_1fr_2fr_max-content_0.5fr]", "text-xl", "md:text-2xl"],
   leftMenu: ["text-left", "gap-7", "items-center", "hidden", "md:flex"],
   logo: ["logo", "font-sans", "pl-4", "xs:pl-10", "mx-auto", "md:text-6xl", "cursor-pointer", "text-4xl", "transition-[color]", "uppercase", "col-span-2"],
@@ -9,3 +15,62 @@ export const HEADER_CLASSES: {[key: string]: string[]} = {
   svgDiv: ["flex", "items-center", "justify-center", "gap-2", "md:gap-4"],
   svgPicture: ["w-12", "h-8", "bg-no-repeat", "bg-center", "transition-[fill]", "header-hovered", "md:h-9"],
 };
+
+const aboutButton = createButton({ 
+  classes: HEADER_CLASSES.menuItem,
+  events: {click: () => {
+    console.log('Go to Page About Us')
+  }},
+  text: 'About Us' 
+});
+
+const logo = createH1({
+  classes: HEADER_CLASSES.logo,
+  events: {click: () => {
+    console.log('Go to Main page');
+  }},
+  text: 'yanki',
+});
+const loginButton = createButton({ classes: [...HEADER_CLASSES.menuItem, "not-logined"],
+  events: {click: () => {
+    console.log('Go to Login Page');
+  }},
+  text: 'log in',
+});
+
+const registrationButton = createButton({ 
+  classes: [...HEADER_CLASSES.menuItem, "not-logined"],
+  events: {click: () => {
+    console.log('Go to Registration Page')
+  }},
+  text: 'Sign up',
+});
+
+const logoutButton = createButton({ classes: [...HEADER_CLASSES.menuItem, "logined"],
+  events: {click: () => {
+    console.log('Go to Main Page & Log Out');
+  }},
+  text: 'log out',
+});
+
+const buttonPerson = createButtonWithSVG(
+  HEADER_CLASSES.svgButtonPerson, 
+  HEADER_CLASSES.svgPicture,
+  '/svg/sprite.svg#person'
+);
+
+const buttonBasket = createButtonWithSVG(
+  HEADER_CLASSES.svgButtonBasket, 
+  HEADER_CLASSES.svgPicture, 
+  '/svg/sprite.svg#basket',
+);
+
+const calalogButton = createButton({ 
+  classes: HEADER_CLASSES.menuItem, 
+  events: {click: () => {
+    console.log('Go to Page Catalog');
+  }},
+  text: 'Catalog',
+});
+
+export { aboutButton, buttonBasket, buttonPerson, calalogButton, HEADER_CLASSES, loginButton, logo, logoutButton, registrationButton };
--- src/components/header/layout.ts
+++ src/components/header/layout.ts
@@ -1,73 +1,36 @@
 import './header.css';
-import { createButtonWithSVG } from '../../utils/create-elements/create-svg-buttom';
 import { 
-  createButton,
-  createDiv,
-  createH1, 
+  createDiv, 
   createHeader, 
   createNav,
- } from '../../utils/create-elements/create-tags';
- import { HEADER_CLASSES } from './constants';
+} from '../../utils/create-elements/create-tags';
+import { aboutButton, 
+  buttonBasket, 
+  buttonPerson, 
+  calalogButton, 
+  HEADER_CLASSES, 
+  loginButton, 
+  logo, 
+  logoutButton, 
+  registrationButton, 
+} from './constants';
 
-const buttonPerson = createButtonWithSVG(
-  HEADER_CLASSES.svgButtonPerson, 
-  HEADER_CLASSES.svgPicture,
-  '/svg/sprite.svg#person');
-
-const buttonBasket = createButtonWithSVG(HEADER_CLASSES.svgButtonBasket, 
-  HEADER_CLASSES.svgPicture, 
-  '/svg/sprite.svg#basket');
- 
-const leftNav = createNav({
+const aboutNAV = createNav({
   children: [
-    createButton({ 
-      classes: HEADER_CLASSES.menuItem, 
-      events: {click: () => {
-        console.log('Go to Page Catalog')}},
-      text: 'Catalog' 
-    }),
-    createButton({ 
-      classes: HEADER_CLASSES.menuItem,
-      events: {click: () => {
-        console.log('Go to Page About Us')
-      }},
-      text: 'About Us' }),
+    calalogButton,
+    aboutButton,
   ],
   classes: HEADER_CLASSES.leftMenu,
 });
 
-const logo = createH1({
-  classes: HEADER_CLASSES.logo,
-  events: {click: () => {
-    console.log('Go to Main page');
-  }},
-  text: 'yanki',
-});
-
-const rightNav = createNav({
+const loginNAV = createNav({
   children: [
-    createButton({ classes: [...HEADER_CLASSES.menuItem, "not-logined"],
-      events: {click: () => {
-        console.log('Go to Login Page')
-      }},
-      text: 'log in' }),
-    createButton({ 
-      classes: [...HEADER_CLASSES.menuItem, "not-logined"],
-      events: {click: () => {
-        console.log('Go to Registration Page')
-      }},
-      text: 'Sign up',
-    }),
-    createButton({ classes: [...HEADER_CLASSES.menuItem, "logined"],
-      events: {click: () => {
-        console.log('Go to Main Page & Log Out');
-      }},
-      text: 'log out',
-    }),
+    loginButton,
+    registrationButton,
+    logoutButton,
   ],
   classes: HEADER_CLASSES.rightMenu,
 });
-
 
 export function createHeaderDOM():HTMLElement {
   buttonPerson.addEventListener("click", () => {
@@ -83,7 +46,7 @@ export function createHeaderDOM():HTMLElement {
   });
 
   const header = createHeader({
-    children: [leftNav, logo, rightNav, iconsDiv],
+    children: [aboutNAV, logo, loginNAV, iconsDiv],
     classes: HEADER_CLASSES.header,
   });
   return header;
