6e619e31c13e40baf957aa8c5a35f0c41e772568
unknown <tarpel.gta@gmail.com>
2025-05-09 15:47:50 +0300

    fix: 25-remove function call from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,5 @@
 import './style.css';
 import { loginUser } from './app/api';
-import { Header } from './components/header';
-import { toggleClassesOnRedirect } from './helpers/toggle-classes-on-redirect';
-
-document.body.append(Header());
-
-//test toggleclasses
-toggleClassesOnRedirect(false, 'basket');
 
 // test
 // delete the lines below
