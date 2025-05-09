7576d06b47030d6b5c6e4df715f83dcafa4a4e75
unknown <tarpel.gta@gmail.com>
2025-05-09 23:45:31 +0300

    style: 26-format code using prettier

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import './style.css';
 import { loginUser } from './app/api';
-import { ErrorPage } from './pages/error';
 
 // test
 // delete the lines below
-ErrorPage();
 try {
   const result = await loginUser('rado@mailinator.com', '1234');
   console.log(result);
--- src/pages/error/lauout.ts
+++ src/pages/error/lauout.ts
@@ -1,23 +1,29 @@
-import { BUTTON } from "../../shared/styles";
-import { createButton, createDiv, createImg, createMain, createP } from "../../utils/create-elements/create-tags";
-import { ERROR, IMAGE_PATH, TEXT } from "./constants";
+import { BUTTON } from '../../shared/styles';
+import { createButton, createDiv, createImg, createMain, createP } from '../../utils/create-elements/create-tags';
+import { ERROR, IMAGE_PATH, TEXT } from './constants';
 
-export function ErrorPage():HTMLElement {
-  // const wrapper = createDiv({ classes: ERROR.image, parent: container, children: Header() });
+export function ErrorPage(): HTMLElement {
   const image = createImg({
-    attributes: { "alt":'jacket 404', "src":IMAGE_PATH, }, 
+    attributes: { alt: 'jacket 404', src: IMAGE_PATH },
     classes: ERROR.image,
   });
 
-  const errorNumber = createP({ classes: ERROR.errorNumber, text: '404' })
-  const errorTitle = createP({ classes: ERROR.errorTitle, text: 'Page not Found' })
+  const errorNumber = createP({ classes: ERROR.errorNumber, text: '404' });
+  const errorTitle = createP({ classes: ERROR.errorTitle, text: 'Page not Found' });
   const errorText = createP({ classes: ERROR.errorText, text: TEXT });
-  const mainButton = createButton({ classes: [...BUTTON.general, ...BUTTON.generalHover, ...BUTTON.generalFocus, "uppercase"], events: {click: ()=> console.log('go to main page')}, text: 'Main page', })
+  const mainButton = createButton({
+    classes: [...BUTTON.general, ...BUTTON.generalHover, ...BUTTON.generalFocus, 'uppercase'],
+    events: { click: () => console.log('go to main page') },
+    text: 'Main page',
+  });
 
-  const infoWrapper = createDiv({ children: [errorNumber, errorTitle, errorText, mainButton], classes: ERROR.infoWrapper, });
+  const infoWrapper = createDiv({
+    children: [errorNumber, errorTitle, errorText, mainButton],
+    classes: ERROR.infoWrapper,
+  });
 
-  const wrapper = createDiv({ children: [image, infoWrapper], classes: ERROR.wrapper});
-  const container = createMain({ children: [wrapper], classes: ERROR.container, parent: document.body});
+  const wrapper = createDiv({ children: [image, infoWrapper], classes: ERROR.wrapper });
+  const container = createMain({ children: [wrapper], classes: ERROR.container, parent: document.body });
 
   return container;
 }
