02b79a29118cfb23a43873784f35f2a262901000
koonukaame <marialezhebokova@yandex.ru>
2025-05-12 02:03:44 +0300

    fix: 23 - fix cloned elements not having event listeners

--- src/components/registration/address-block.ts
+++ src/components/registration/address-block.ts
@@ -1,3 +1,4 @@
+import { cloneInputs } from '../../helpers/clone-input';
 import { REGISTRATION } from '../../pages/registration/constants';
 import { CHECKBOX } from '../../shared/styles';
 import { createDiv } from '../../utils/create-elements/create-tags';
@@ -11,26 +12,9 @@ export function createAddressBlock(
 ): HTMLFieldSetElement {
   const title = createLegend({ text: blockTitle });
 
-  const clonedContainer = inputs.map((container) => {
-    const clonedElement = container.cloneNode(true);
-    if (!(clonedElement instanceof HTMLElement)) {
-      throw new TypeError('Cloned node is not an HTMLElement');
-    }
+  const clonedInputs = cloneInputs(inputs, addressName);
 
-    const input = clonedElement.querySelector('input');
-    if (input) {
-      const originalName = input.getAttribute('name');
-      input.setAttribute('name', `${originalName}-${addressName}`);
-    }
-    if (clonedElement instanceof HTMLSelectElement) {
-      const originalName = clonedElement.getAttribute('name');
-      clonedElement.setAttribute('name', `${originalName}-${addressName}`);
-    }
-
-    return clonedElement;
-  });
-
-  const inputsContainer = createDiv({ children: clonedContainer, classes: REGISTRATION.inputsContainer });
+  const inputsContainer = createDiv({ children: clonedInputs, classes: REGISTRATION.inputsContainer });
 
   const defaultCheckboxLabel = createLabel({
     attributes: { for: defaultAddressCheckbox.getAttribute('id') || '' },
---
+++ src/helpers/clone-input.ts
@@ -0,0 +1,58 @@
+import { REGISTRATION_INPUTS_CONFIG } from '../pages/registration/constants';
+
+export function cloneInputs(inputs: HTMLElement[], addressName: string): HTMLElement[] {
+  return inputs.map((container) => {
+    const clonedElement = container.cloneNode(true);
+    if (!(clonedElement instanceof HTMLElement)) {
+      throw new TypeError('cloned node is not an HTMLElement');
+    }
+
+    modifyInputElementName(clonedElement, addressName);
+    modifySelectElementName(clonedElement, addressName);
+
+    return clonedElement;
+  });
+}
+
+function addInputEvents(input: HTMLInputElement, originalName: string): void {
+  const configEntry = Object.entries(REGISTRATION_INPUTS_CONFIG).find(
+    ([, value]) => value.attributes?.name === originalName,
+  );
+
+  if (configEntry) {
+    const [, config] = configEntry;
+    if (config.events) {
+      for (const [eventName, handler] of Object.entries(config.events)) {
+        input.addEventListener(eventName, handler);
+      }
+    }
+  }
+}
+
+function modifyInputElementName(element: HTMLElement, addressName: string): void {
+  const input = element.querySelector('input');
+  if (!input) {
+    return;
+  }
+
+  const originalName = input.getAttribute('name');
+  if (!originalName) {
+    throw new Error('input element does not have name attribute');
+  }
+
+  input.setAttribute('name', `${originalName}-${addressName}`);
+  addInputEvents(input, originalName);
+}
+
+function modifySelectElementName(element: HTMLElement, addressName: string): void {
+  if (!(element instanceof HTMLSelectElement)) {
+    return;
+  }
+
+  const originalName = element.getAttribute('name');
+  if (!originalName) {
+    throw new Error('select does not have name attribute');
+  }
+
+  element.setAttribute('name', `${originalName}-${addressName}`);
+}
