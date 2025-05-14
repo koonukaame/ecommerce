7228dfaa3d4e29ead7ccc899b7710594ce8be07b
Alex <strelchenko.aleksandr@yandex.ru>
2025-05-14 13:08:45 +0300

    feat: get form fields with querySelector and form() for tests

--- src/shared/components/button.ts
+++ src/shared/components/button.ts
@@ -2,6 +2,8 @@ import type { Options } from '../../utils/create-elements/types';
 
 import { createButton } from '../../utils/create-elements/create-tags';
 import { BUTTON } from '../styles';
+import { form } from '../../components/registration/form';
+import { registrationErrors } from '../../app/state/registration';
 
 type Button = Record<'login' | 'main' | 'registration', ButtonProps>;
 type ButtonProps = Omit<Options<'button'>, 'children' | 'parent' | 'tag'>;
@@ -30,7 +32,42 @@ export const BUTTONS_CONFIG: Button = {
     },
     classes: [...BUTTON.general, ...BUTTON.generalHover, ...BUTTON.generalFocus],
     events: {
-      click: () => console.log('clicked register'),
+      click: () => {
+
+        // доступ к выпадающему списку стран в форме черзе document.querySelector('form')
+        let form = document.querySelector('form')
+
+        if (!(form instanceof HTMLFormElement)) {
+          return;
+        }
+
+        const shippingCountryElement = form.elements.namedItem('shippingCountry');
+        const billingCountryElement = form.elements.namedItem('billingCountry');
+
+        if (
+          shippingCountryElement instanceof HTMLSelectElement &&
+          billingCountryElement instanceof HTMLSelectElement
+        ) {
+          registrationErrors.shippingCountry = Boolean(shippingCountryElement.value);
+          registrationErrors.billingCountry = Boolean(billingCountryElement.value);
+          console.log(registrationErrors);
+        }
+
+
+
+        // доступ к выпадающему списку стран в форме черeз импорт функции form()
+        // const shippingCountryElement = form().elements.namedItem('shippingCountry');
+        // const billingCountryElement = form().elements.namedItem('billingCountry');
+
+        // if (
+        //   shippingCountryElement instanceof HTMLSelectElement &&
+        //   billingCountryElement instanceof HTMLSelectElement
+        // ) {
+        //   registrationErrors.shippingCountry = Boolean(shippingCountryElement.value);
+        //   registrationErrors.billingCountry = Boolean(billingCountryElement.value);
+        //   console.log(registrationErrors);
+        // }
+      },
     },
     text: 'Register',
   },
