29bf48c36f87d6e897495d85df959ef75bd3140a
Alex <strelchenko.aleksandr@yandex.ru>
2025-05-14 12:55:31 +0300

    feat: create state for registration form

--- src/app/state/registration.ts
+++ src/app/state/registration.ts
@@ -1,5 +1,19 @@
-export const registrationErrors = {
-  city: undefined,
+type RegistrationErrors = Record<string, boolean | undefined>;
+
+export const registrationErrors: RegistrationErrors = {
+  billingCity: undefined,
+  billingCountry: undefined,
+  billingPostalCode: undefined,
+  billingStreet: undefined,
+  dateOfBirth: undefined,
+  defaultBilling: false,
+  defaultShipping: false,
   email: undefined,
+  firstName: undefined,
+  lastName: undefined,
   password: undefined,
-}
+  shippingCity: undefined,
+  shippingCountry: undefined,
+  shippingPostalCode: undefined,
+  shippingStreet: undefined,
+};
