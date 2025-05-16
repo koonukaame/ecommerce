c2bbe4250e76e067c0de2dcc46d774d0d32ff123
koonukaame <marialezhebokova@yandex.ru>
2025-05-16 19:01:01 +0300

    refactor: 74 - refactor logo

--- src/components/header/layout.ts
+++ src/components/header/layout.ts
@@ -1,6 +1,8 @@
 import './header.css';
-import { logo } from '../../shared/components/logo/logo';
-import { createDiv, createHeader, createNav } from '../../utils/create-elements/create-tags';
+import { Page } from '../../app/constants';
+import { changePath } from '../../app/router/handlers';
+import { LOGO_CLASSES } from '../../shared/styles';
+import { createDiv, createH1, createHeader, createNav } from '../../utils/create-elements/create-tags';
 import { about, basket, catalog, login, logout, person, registration } from './buttons';
 import { HEADER_CLASSES } from './constants';
 
@@ -8,6 +10,14 @@ export function Header(): void {
   const aboutNav = createNav({
     children: [catalog, about],
     classes: HEADER_CLASSES.leftMenu,
+  });
+
+  const logo = createH1({
+    classes: LOGO_CLASSES,
+    events: {
+      click: changePath(Page.main),
+    },
+    text: 'yanki',
   });
 
   const loginNav = createNav({
