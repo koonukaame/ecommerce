b367df0bcadcbfc204c3127a1507028bdf6ac998
koonukaame <marialezhebokova@yandex.ru>
2025-05-17 18:39:58 +0300

    feat: 87 - connect address inputs to event emitter

---
+++ src/helpers/address-emitter.ts
@@ -0,0 +1,73 @@
+import type { FieldState } from '../app/types';
+import type { WrappedInput } from '../shared/components/input';
+
+import { isSameAddress, registrationState } from '../app/state/registration';
+import { CustomEventEmitter } from '../utils/event-emitter';
+
+export const sameAddressEmitter = new CustomEventEmitter();
+
+export function emitAdressInputs(
+  cityInput: WrappedInput,
+  postalCodeInput: WrappedInput,
+  streetInput: WrappedInput,
+  countriesSelect: HTMLSelectElement,
+  addressType: 'billing' | 'shipping',
+): void {
+  if (addressType === 'shipping') {
+    streetInput.input.addEventListener('input', (event) => emitFieldChange(event, 'syncStreet'));
+    cityInput.input.addEventListener('input', (event) => emitFieldChange(event, 'syncCity'));
+    postalCodeInput.input.addEventListener('input', (event) => emitFieldChange(event, 'syncPostalCode'));
+    countriesSelect.addEventListener('input', (event) => emitFieldChange(event, 'syncCountry'));
+  }
+}
+
+export function subscribeToAddressInputs(
+  cityInput: WrappedInput,
+  postalCodeInput: WrappedInput,
+  streetInput: WrappedInput,
+  countriesSelect: HTMLSelectElement,
+  addressType: 'billing' | 'shipping',
+): void {
+  sameAddressEmitter.subscribe('sameAddress', (isSame) => {
+    if (typeof isSame !== 'boolean') {
+      return;
+    }
+    isSameAddress.value = isSame;
+    if (isSame && addressType === 'billing') {
+      cityInput.input.value = registrationState.shippingCity.value;
+      postalCodeInput.input.value = registrationState.shippingPostalCode.value;
+      streetInput.input.value = registrationState.shippingStreet.value;
+      countriesSelect.value = registrationState.shippingCountry.value;
+    }
+  });
+
+  if (addressType === 'billing') {
+    syncInputValue('syncStreet', registrationState.billingStreet, streetInput);
+    syncInputValue('syncCity', registrationState.billingCity, cityInput);
+    syncInputValue('syncPostalCode', registrationState.billingPostalCode, postalCodeInput);
+    syncInputValue('syncCountry', registrationState.billingCountry, countriesSelect);
+  }
+}
+
+function emitFieldChange(event: Event, eventName: string): void {
+  const target = event.target;
+  if (target instanceof HTMLInputElement || target instanceof HTMLSelectElement) {
+    sameAddressEmitter.emit(eventName, target.value);
+  } else {
+    throw new TypeError('Input is not an HTMLInputElement or HTMLSelectElement');
+  }
+}
+
+function syncInputValue(eventName: string, fieldState: FieldState, input: HTMLSelectElement | WrappedInput): void {
+  sameAddressEmitter.subscribe(eventName, (value: unknown) => {
+    if (typeof value !== 'string' || !value || !isSameAddress.value) {
+      return;
+    }
+    if (input instanceof HTMLSelectElement) {
+      input.value = value;
+    } else {
+      input.input.value = value;
+    }
+    fieldState.value = value;
+  });
+}
