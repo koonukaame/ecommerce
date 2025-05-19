5518632162284d453970e666ffce98eb1682acf3
Tanya <tarpel.gta@gmail.com>
2025-05-19 08:34:00 +0300

    chore: 99-install jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   build: { outDir: 'dist' },
   plugins: [tailwindcss()],
   test: {
+    environment: 'jsdom',
     coverage: {
       exclude: [
         ...configDefaults.exclude,
