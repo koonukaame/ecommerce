a9fe66cb4f072851a8eb8ad2b829b60417bd9cf4
Alex <strelchenko.aleksandr@yandex.ru>
2025-05-21 17:23:22 +0300

    feat: 117-add getProductById function in index.ts

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,3 +1,4 @@
 export { loginUser } from './login-user';
 export { registerUser } from './register-user';
 export { getAllProducts } from './get-all-products';
+export { getProductById } from './get-product-by-id';
