855c5dc0f77ba78c5c4f126b64a601623df1dcaa
Tanya <tarpel.gta@gmail.com>
2025-05-24 00:57:41 +0300

    feat: 121-add product category in breadcrumbs

--- src/components/product/breadcrumbs.ts
+++ src/components/product/breadcrumbs.ts
@@ -1,27 +1,45 @@
+import type { LocalizedString } from '@commercetools/platform-sdk';
 import { Page } from '../../app/constants';
 import { changePath } from '../../app/router/handlers';
 import { createButton, createDiv, createSpan } from '../../utils/create-elements/create-tags';
 
-const BREADSCRUMS_PARTS = {
-  'Main page': () => changePath(Page.main)(),
-  Catalog: () => changePath(Page.catalog)(),
-  'Category name': () => console.log('go to category'),
-  'Product name': () => console.log('go to product'),
-};
+type CrumbEntry = [text: string | LocalizedString, callback: () => void];
 
-export function breadcrumbs(): HTMLElement {
-  const container = createDiv({ classes: ['w-full', 'px-5'] });
-  Object.entries(BREADSCRUMS_PARTS).map(([linkName, getCallback], index, array) => {
+const BREADCRUMBS_PARTS: CrumbEntry[] = [
+  ['Main page', () => changePath(Page.main)()],
+  ['Catalog', () => changePath(Page.catalog)()],
+];
+
+export function BreadCrumbs(entries: CrumbEntry[]): HTMLElement {
+  const container = createDiv({ classes: ['w-full', 'md:px-5'] });
+  [...BREADCRUMBS_PARTS, ...entries].map(([linkName, callback], index, array) => {
     createButton({
-      classes: ['p-3', 'cursor-pointer', 'md:text-xl', 'hover:text-(--hover-link-header)', 'transition-[color]'],
-      text: linkName,
-      events: { click: () => getCallback() },
+      classes: [
+        'p-1.5',
+        'md:p-3',
+        'cursor-pointer',
+        'md:text-xl',
+        'hover:text-(--hover-link-header)',
+        'transition-[color]',
+      ],
+      text: typeof linkName === 'string' ? linkName : linkName['en'],
+      events: { click: callback },
       parent: container,
     });
 
     if (index < array.length - 1) {
       createSpan({
-        classes: ['bg-no-repeat', 'bg-contain', 'h-5', 'mx-6', 'w-5', 'inline-block', 'bg-[url("./svg/greater.svg")]'],
+        classes: [
+          'bg-no-repeat',
+          'bg-contain',
+          'h-3',
+          'md:h-5',
+          'md:mx-6',
+          'w-2',
+          'md:w-5',
+          'inline-block',
+          'bg-[url("./svg/greater.svg")]',
+        ],
         parent: container,
       });
     }
--- src/components/product/layout.ts
+++ src/components/product/layout.ts
@@ -1,16 +1,40 @@
 import { createDiv } from '../../utils/create-elements/create-tags';
 import type { ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk';
-import { breadcrumbs } from './breadcrumbs';
+import { BreadCrumbs } from './breadcrumbs';
+import { ProductSlider } from './slider';
 
 export function productLayout(data: ProductProjectionPagedQueryResponse): HTMLDivElement {
   console.log(data);
-  const breadcrumb = breadcrumbs();
-  const slider = createDiv({ classes: ['w-[50%]'] });
+
+  const category = data.results[0].categories[0].obj?.name ?? 'category';
+  const product = data.results[0].name;
+
+  const breadcrumb = BreadCrumbs([
+    [
+      category,
+      () => {
+        console.log('go to category');
+      },
+    ],
+    [
+      product,
+      () => {
+        console.log('go to product');
+      },
+    ],
+  ]);
+
+  const slider = ProductSlider(data.results[0].masterVariant.images);
   const description = createDiv({ classes: ['w-[50%]'] });
 
-  const layout = createDiv({
-    children: [breadcrumb, slider, description],
+  const productContainer = createDiv({
+    children: [slider, description],
     classes: ['w-full'],
+  });
+
+  const layout = createDiv({
+    children: [breadcrumb, productContainer],
+    classes: ['w-full', 'flex', 'flex-col'],
   });
 
   return layout;
