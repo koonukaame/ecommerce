22aed3605444a9025919e5a74789b6ee04933841
Alex <strelchenko.aleksandr@yandex.ru>
2025-05-23 10:52:47 +0300

    feat: 124-update ProfilePage function

--- src/pages/profile/constants.ts
+++ src/pages/profile/constants.ts
@@ -1,5 +1,127 @@
-export const PROFILE_CLASSES = {};
+import { dateOfBirthValidation, inputValidation } from '../../utils/validation/profile/input-validation';
+import { ERROR_MESSAGES, REGEX } from '../../shared/constants';
+import { buttonEmitter } from '../../helpers/buttons-emitter';
 
-export const PROFILE_CONFIG = {};
+export const PROFILE_CLASSES = {
+  baseButton: ['w-20', 'h-10', 'text-lg', 'text-white', 'rounded', 'absolute', 'bg-blue-200'],
+  buttonEdit: ['cursor-pointer', 'top-1', 'right-1', 'bg-blue-600'],
+  buttonSave: ['block', 'bottom-1', 'left-3'],
+  buttonCancel: ['block', 'bottom-1', 'left-25'],
+  personalInfoSection: ['relative', 'w-full', 'h-[310px]', 'border', 'border-black', 'pl-[10px]', 'text-[15px]'],
+  label: ['text-[20px]', 'block'],
+  wrapper: [
+    'flex',
+    'flex-col',
+    'max-w-[600px]',
+    'w-full',
+    'items-center',
+    'gap-[30px]',
+    'p-3',
+    'border',
+    'border-[#252525]/50',
+  ],
+  input: ['mt-5', 'border', 'border-gray-300', 'rounded', 'p-2', 'bg-gray-100', 'text-gray-500'],
+};
 
-export const BUTTONS_CONFIG = {};
+export const PROFILE_CONFIG = {
+  birthdate: {
+    attributes: {
+      name: 'dateOfBirth',
+      type: 'date',
+      disabled: 'true',
+    },
+    classes: PROFILE_CLASSES.input,
+    events: {
+      change: (event: Event) => {
+        dateOfBirthValidation(event, ERROR_MESSAGES.AGE_RESTRICTION);
+      },
+    },
+  },
+  firstname: {
+    attributes: {
+      autocomplete: 'true',
+      name: 'firstName',
+      placeholder: 'Enter first name*',
+      type: 'text',
+      disabled: 'true',
+    },
+    classes: PROFILE_CLASSES.input,
+    events: {
+      input: (event: Event) => {
+        inputValidation(event, REGEX.GENERAL, ERROR_MESSAGES.FIRST_NAME);
+      },
+    },
+  },
+  lastname: {
+    attributes: {
+      name: 'lastName',
+      placeholder: 'Enter last name*',
+      type: 'text',
+      disabled: 'true',
+    },
+    classes: PROFILE_CLASSES.input,
+    events: {
+      input: (event: Event) => {
+        inputValidation(event, REGEX.GENERAL, ERROR_MESSAGES.LAST_NAME);
+      },
+    },
+  },
+};
+
+export const BUTTONS_CONFIG = {
+  edit: {
+    classes: [...PROFILE_CLASSES.baseButton, ...PROFILE_CLASSES.buttonEdit],
+    events: {
+      click: async () => {
+        buttonEmitter.emit('editBtnClick');
+      },
+    },
+    text: 'Edit',
+  },
+  save: {
+    attributes: {
+      disabled: 'true',
+    },
+    classes: [...PROFILE_CLASSES.baseButton, ...PROFILE_CLASSES.buttonSave],
+    events: {
+      click: () => {
+        buttonEmitter.emit('saveBtnClick');
+        buttonEmitter.emit('updateUserData');
+
+        // const isFormValid: boolean = validateLoginForm();
+
+        // if (!isFormValid) {
+        //   return;
+        // }
+
+        // const { email, password } = registrationState;
+
+        // try {
+        //   const response = await loginUser(email.value, password.value);
+
+        //   if ('id' in response) {
+        //     createPopupMessage(`Welcome back, ${response.firstName}!`, true);
+        //     return;
+        //   }
+
+        //   const message = response.message;
+
+        //   createPopupMessage(message, false);
+        // } catch {
+        //   createPopupMessage('Failed to update Personal Data', false);
+        // }
+      },
+    },
+    text: 'Save',
+  },
+  cancel: {
+    attributes: {
+      disabled: 'true',
+    },
+    classes: [...PROFILE_CLASSES.baseButton, ...PROFILE_CLASSES.buttonCancel],
+    events: {
+      click: () => buttonEmitter.emit('cancelBtnClick'),
+    },
+    text: 'Cancel',
+  },
+};
--- src/pages/profile/layout.ts
+++ src/pages/profile/layout.ts
@@ -1,3 +1,66 @@
-export async function ProfilePage(): Promise<void> {
-  console.log('created ProfilePage function');
+import { container } from '../../shared/components/container';
+import { getUserInfo } from '../../app/api';
+import { getAuthToken } from '../../app/ecommerce/get-auth-token';
+import type { FetchError } from '../../app/types';
+import { activateButtonEmitter } from '../../helpers/buttons-emitter';
+import { createWrappedInput } from '../../shared/components/input';
+import { HEADER2 } from '../../shared/styles';
+import { createButton, createDiv, createH2 } from '../../utils/create-elements/create-tags';
+import { BUTTONS_CONFIG, PROFILE_CLASSES, PROFILE_CONFIG } from './constants';
+
+export async function ProfilePage(): Promise<HTMLElement | FetchError> {
+  //! Delete in the future when I save token in local/session storage
+  const token = await getAuthToken('ivanIvanov@yandex.ru', 'Ivan12345');
+
+  if (typeof token !== 'string') {
+    return { message: 'Failed to get token' };
+  }
+
+  const user = await getUserInfo(token);
+
+  if (!('id' in user)) {
+    return { message: 'Failed to get Personal Data' };
+  }
+
+  // updatePersonalDataState(user.firstName, user.lastName, user.dateOfBirth)
+
+  const title = createH2({ classes: HEADER2.general, text: 'Personal Profile' });
+
+  const personalInfoSection = createDiv({ classes: PROFILE_CLASSES.personalInfoSection });
+
+  const firstNameInput = createWrappedInput(PROFILE_CONFIG.firstname);
+  firstNameInput.input.value = user.firstName || 'undefined';
+
+  const lastNameInput = createWrappedInput(PROFILE_CONFIG.lastname);
+  lastNameInput.input.value = user.lastName || 'undefined';
+
+  const birthDateInput = createWrappedInput(PROFILE_CONFIG.birthdate);
+  birthDateInput.input.value = user.dateOfBirth || 'undefined';
+
+  const editButton = createButton(BUTTONS_CONFIG.edit);
+  const saveButton = createButton(BUTTONS_CONFIG.save);
+  const cancelButton = createButton(BUTTONS_CONFIG.cancel);
+
+  activateButtonEmitter(
+    editButton,
+    saveButton,
+    cancelButton,
+    firstNameInput.input,
+    lastNameInput.input,
+    birthDateInput.input,
+  );
+
+  personalInfoSection.append(
+    firstNameInput.container,
+    lastNameInput.container,
+    birthDateInput.container,
+    editButton,
+    saveButton,
+    cancelButton,
+  );
+
+  const wrapper = createDiv({ classes: PROFILE_CLASSES.wrapper, children: [title, personalInfoSection] });
+
+  container.append(wrapper);
+  return container;
 }
