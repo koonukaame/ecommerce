820b0145725fe9dc3ee560b19cb1e73e060b4dbe
Tanya <tarpel.gta@gmail.com>
2025-05-24 01:06:28 +0300

    feat: 121-add slider

---
+++ src/components/product/slider.ts
@@ -0,0 +1,54 @@
+import { createDiv, createImg } from '../../utils/create-elements/create-tags';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import type { Image } from '@commercetools/platform-sdk';
+import 'swiper/swiper-bundle.css';
+import './slider.css';
+
+Swiper.use([Navigation, Pagination]);
+
+export function ProductSlider(slides: Image[] | undefined): HTMLElement {
+  if (!slides) {
+    return createDiv({ classes: ['w-[50%]', 'px-5'], text: 'SLIDER' });
+  }
+
+  const container = createDiv({ classes: ['swiper-container', 'pb-[30px]'] });
+  const wrapper = createDiv({ classes: ['swiper-wrapper'], parent: container });
+
+  slides.map((url, index) => {
+    const slide = createDiv({
+      classes: ['p-3', 'cursor-pointer', 'swiper-slide', 'text-center'],
+      parent: wrapper,
+    });
+
+    createImg({
+      classes: ['h-auto', 'max-w-[100%]'],
+      attributes: { src: url.url, alt: `Slide ${index}` },
+      parent: slide,
+    });
+  });
+
+  createDiv({ classes: ['swiper-button-prev'], parent: container });
+
+  createDiv({ classes: ['swiper-button-next'], parent: container });
+
+  createDiv({ classes: ['swiper-pagination'], parent: container });
+
+  // createDiv({ classes: ['swiper-scrollbar'], parent: container});
+
+  setTimeout(() => {
+    new Swiper('.swiper-container', {
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }, 0);
+
+  return container;
+}
