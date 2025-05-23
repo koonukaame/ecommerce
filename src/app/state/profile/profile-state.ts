eeb667e1d44f6cfc48de730828c5659342a63866
Alex <strelchenko.aleksandr@yandex.ru>
2025-05-23 10:43:59 +0300

    feat: 124-create state profileDataState object to store personal data for validation

---
+++ src/app/state/profile/profile-state.ts
@@ -0,0 +1,19 @@
+import type { ProfileDataState } from '../../types';
+
+export const profileDataState: ProfileDataState = {
+  firstName: {
+    error: undefined,
+    rawValue: '',
+    value: '',
+  },
+  lastName: {
+    error: undefined,
+    rawValue: '',
+    value: '',
+  },
+  dateOfBirth: {
+    error: undefined,
+    rawValue: '',
+    value: '',
+  },
+};
