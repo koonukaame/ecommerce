1a2ae58e3a89d6d4dfabfff51f33c86cc70f2e75
koonukaame <marialezhebokova@yandex.ru>
2025-05-23 18:37:08 +0300

    refactor: remove shared components for name and description

--- src/shared/components/product-description.ts
+++
@@ -1,8 +0,0 @@
-import type { ProductProjection } from '@commercetools/platform-sdk';
-import { createSpan } from '../../utils/create-elements/create-tags';
-
-export function createProductDescription(product: ProductProjection, classes: string[]): HTMLSpanElement {
-  const description = createSpan({ text: product.description?.en || 'Just a cool product for you!', classes });
-
-  return description;
-}
--- src/shared/components/product-name.ts
+++
@@ -1,8 +0,0 @@
-import type { ProductProjection } from '@commercetools/platform-sdk';
-import { createSpan } from '../../utils/create-elements/create-tags';
-
-export function createProductName(product: ProductProjection, classes: string[]): HTMLSpanElement {
-  const name = createSpan({ text: product.name.en, classes });
-
-  return name;
-}
