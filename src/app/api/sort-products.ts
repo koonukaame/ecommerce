74f81b2ebdb2c77b6ea8437c3989a9e8c26269bd
koonukaame <marialezhebokova@yandex.ru>
2025-05-25 19:28:30 +0300

    feat: 133 - create endpoint for sorting

---
+++ src/app/api/sort-products.ts
@@ -0,0 +1,36 @@
+import { getAnonymousToken } from '../ecommerce/get-anonymous-token';
+import type { ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk';
+import type { FetchError } from '../types';
+import { API_URL, PROJECT_KEY } from '../constants';
+
+export async function sortProducts(sort: string): Promise<ProductProjectionPagedQueryResponse | FetchError> {
+  const token = await getAnonymousToken();
+
+  try {
+    const response = await fetch(
+      `${API_URL}/${PROJECT_KEY}/product-projections/search?sort=${encodeURIComponent(sort)}&limit=20`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    if (!response.ok) {
+      const error = await response.json();
+
+      return { message: error.message || 'Failed to fetch a product' };
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      return { message: error.message };
+    }
+
+    return { message: 'Unexpected error during fetching a product' };
+  }
+}
