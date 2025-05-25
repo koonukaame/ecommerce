1e87d4e6a1df93248c4c961c7819bd3cedfa8be8
koonukaame <marialezhebokova@yandex.ru>
2025-05-25 13:25:24 +0300

    refactor: 129 - move await statement inside try catch block

--- src/helpers/fetch-product-cards.ts
+++ src/helpers/fetch-product-cards.ts
@@ -2,9 +2,9 @@ import { getAllProducts } from '../app/api';
 import { createProductList } from '../components/catalog/product-list';
 
 export async function fetchProductCards(layout: HTMLDivElement): Promise<void> {
-  const data = await getAllProducts();
-
   try {
+    const data = await getAllProducts();
+
     if ('results' in data) {
       const cards = createProductList(data);
       layout.append(cards);
