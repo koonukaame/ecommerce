711d678ac1bc0091e442f29bdcdcffef05536058
Tanya <tarpel.gta@gmail.com>
2025-05-25 13:41:57 +0300

    refactor: 121-update imports & types

--- src/components/product/layout.ts
+++ src/components/product/layout.ts
@@ -1,40 +1,58 @@
 import { createDiv } from '../../utils/create-elements/create-tags';
-import type { ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk';
-import { BreadCrumbs } from './breadcrumbs';
+import type { Image, LocalizedString, ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk';
+import { BreadCrumbs, type CrumbEntry } from '../../shared/components/breadcrumbs';
 import { ProductSlider } from './slider';
+import { ProductDescription } from './description';
+
+export type ProductInfo = {
+  name: string;
+  category: LocalizedString | string;
+  slug: LocalizedString;
+  description: string;
+  price: number;
+  discountPrice: number;
+  photo: Image[];
+};
 
 export function productLayout(data: ProductProjectionPagedQueryResponse): HTMLDivElement {
   console.log(data);
 
-  const category = data.results[0].categories[0].obj?.name ?? 'category';
-  const product = data.results[0].name;
+  const productInfo: ProductInfo = {
+    name: data.results[0].name.en,
+    category: data.results[0].categories[0].obj?.name ?? 'category',
+    slug: data.results[0].slug,
+    description: data.results[0].description?.en ?? '',
+    price: data?.results?.[0]?.masterVariant?.prices?.[0].value.centAmount ?? 0,
+    discountPrice: data?.results?.[0]?.masterVariant?.prices?.[0].discounted?.value.centAmount ?? 0,
+    photo: data.results[0].masterVariant.images ?? [],
+  };
 
-  const breadcrumb = BreadCrumbs([
+  const breadCrumbParameters: CrumbEntry[] = [
     [
-      category,
+      productInfo.category,
       () => {
         console.log('go to category');
       },
     ],
     [
-      product,
+      productInfo.name,
       () => {
         console.log('go to product');
       },
     ],
-  ]);
+  ];
 
-  const slider = ProductSlider(data.results[0].masterVariant.images);
-  const description = createDiv({ classes: ['w-[50%]'] });
-
+  const breadcrumb = BreadCrumbs(breadCrumbParameters);
+  const slider = ProductSlider(productInfo.photo);
+  const description = ProductDescription(productInfo);
   const productContainer = createDiv({
     children: [slider, description],
-    classes: ['w-full'],
+    classes: ['max-w-[1200px]', 'flex', 'flex-col', 'md:flex-row'],
   });
 
   const layout = createDiv({
     children: [breadcrumb, productContainer],
-    classes: ['w-full', 'flex', 'flex-col'],
+    classes: ['w-full', 'flex', 'flex-col', 'items-center'],
   });
 
   return layout;
