495df030a50d1f249a41d36b7dde9cdf263a0f20
Tanya <tarpel.gta@gmail.com>
2025-05-27 01:27:39 +0300

    refactor: 121-implement error page for not found product

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -1,14 +1,15 @@
 import { container } from '../../shared/components/container';
 import { productLayout } from '../../components/product/layout';
 import { getProductById } from '../../app/api/get-product-by-id';
-import { createInfoMessage } from '../../shared/components/info-message';
 import { isFetchError } from '../../app/api/is-fetch-error';
+import { changePath } from '../../app/router/handlers';
+import { Page } from '../../app/constants';
 
 export async function ProductPage(slug = 'silver-fox-fur-coat'): Promise<HTMLElement | void> {
   const data = await getProductById(slug);
 
   if (isFetchError(data) || data.count === 0) {
-    createInfoMessage(container, 'Product Not Found');
+    changePath(Page.error)();
     return;
   }
   const layout = productLayout(data);
