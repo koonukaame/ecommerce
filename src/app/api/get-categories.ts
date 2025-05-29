80a4ce2d56465928f6804ab94cc48a00afd7ab75
koonukaame <marialezhebokova@yandex.ru>
2025-05-29 14:50:46 +0300

    feat: 141 - add endpoint for getting cetegories

---
+++ src/app/api/get-categories.ts
@@ -0,0 +1,35 @@
+import type { CategoryPagedQueryResponse } from '@commercetools/platform-sdk';
+import type { FetchError } from '../types';
+import { API_URL, PROJECT_KEY } from '../constants';
+import { getAnonymousToken } from '../ecommerce/get-anonymous-token';
+
+export async function getCategories(): Promise<CategoryPagedQueryResponse | FetchError> {
+  // TODO! in the future use local/session storage to get the token
+  const token = await getAnonymousToken();
+
+  try {
+    const response = await fetch(`${API_URL}/${PROJECT_KEY}/categories`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+
+      return { message: error.message || 'Failed to fetch products' };
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      return { message: error.message };
+    }
+
+    return { message: 'Unexpected error during fetching products' };
+  }
+}
