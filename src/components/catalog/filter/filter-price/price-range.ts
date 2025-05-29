a8acf477513a54b40ac34e502112b36caf6eb965
koonukaame <marialezhebokova@yandex.ru>
2025-05-29 19:56:42 +0300

    refactor: 139 - move styles import to the top of the page

--- src/components/catalog/filter/filter-price/price-range.ts
+++ src/components/catalog/filter/filter-price/price-range.ts
@@ -1,7 +1,7 @@
-import noUiSlider, { type API } from 'nouislider';
 import 'nouislider/dist/nouislider.css';
-import { handlePriceFilterChange } from '../../../../utils/query-handlers/products-price-filter';
 import './style.css';
+import noUiSlider, { type API } from 'nouislider';
+import { handlePriceFilterChange } from '../../../../utils/query-handlers/products-price-filter';
 import { CustomEventEmitter } from '../../../../utils/event-emitter';
 
 export const clearPriceEmitter = new CustomEventEmitter();
--- src/components/catalog/layout.ts
+++ src/components/catalog/layout.ts
@@ -1,9 +1,9 @@
+import './style.css';
 import { createDiv } from '../../utils/create-elements/create-tags';
 import { fetchProductCards } from '../../helpers/fetch-product-cards';
 import { CATALOG } from '../../pages/catalog/constants';
 import { createSearchSortWrapper } from './search-sort-wrapper';
 import { createFilterComponent } from './filter/filter-price/filter-component';
-import './style.css';
 
 export async function catalogLayout(): Promise<HTMLDivElement> {
   const layout = createDiv({ classes: CATALOG.wrapper });
