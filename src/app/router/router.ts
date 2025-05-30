ba3ba8cb027d532710e2bb12881704dda48306c4
Tanya <tarpel.gta@gmail.com>
2025-05-30 12:02:52 +0300

    refactor: 122-router

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -1,6 +1,6 @@
 import { appState } from '../app-state';
 import { Page } from '../constants';
-import { checkRenderPage } from './handlers';
+import { changePath, checkRenderPage } from './handlers';
 import { renderPage } from './render-page';
 
 export function router(): void {
@@ -8,11 +8,17 @@ export function router(): void {
     const hash = globalThis.location.hash.slice(1).trim();
     const currentPage = checkRenderPage(hash);
 
-    if (appState.currentPage !== currentPage) {
+    if (appState.currentPage === currentPage) {
+      if (currentPage === Page.catalog || currentPage === Page.product) {
+        renderPage(currentPage);
+      }
+    } else {
       appState.currentPage = currentPage;
+
+      if (currentPage === Page.error) {
+        changePath(Page.error)();
+      }
       renderPage(currentPage);
     }
-
-    globalThis.location.hash = appState.currentPage === Page.error ? `#${hash}` : `#${appState.currentPage}`;
   });
 }
