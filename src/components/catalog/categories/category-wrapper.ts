601ef3baea33ee031515d0249d195121bb299c2b
koonukaame <marialezhebokova@yandex.ru>
2025-05-31 15:16:30 +0300

    refactor: 141 - update category wrapper with correct buttons appending logic

--- src/components/catalog/categories/category-wrapper.ts
+++ src/components/catalog/categories/category-wrapper.ts
@@ -5,25 +5,33 @@ import { CustomEventEmitter } from '../../../utils/event-emitter';
 
 export const categoryEventEmitter = new CustomEventEmitter();
 
-export function createCategoryWrapper(categories: Category[]): HTMLDivElement {
+export function createCategoryWrapper(
+  categories: Category[],
+  categoryId?: string,
+  subcategoryId?: string,
+): HTMLDivElement {
   const wrapper = createDiv({});
 
-  const rootCategories = categories.filter((category) => !category.parent);
+  if (subcategoryId) {
+    return wrapper;
+  }
 
+  if (categoryId) {
+    const subcategories = categories.filter((category) => category.parent?.id === categoryId);
+
+    for (const subcategory of subcategories) {
+      const button = createCategoryButton(subcategory);
+      wrapper.append(button);
+    }
+
+    return wrapper;
+  }
+
+  const rootCategories = categories.filter((category) => !category.parent);
   for (const category of rootCategories) {
     const button = createCategoryButton(category);
     wrapper.append(button);
   }
-
-  categoryEventEmitter.subscribe('subcategories', (subcategories: unknown) => {
-    if (Array.isArray(subcategories)) {
-      wrapper.replaceChildren();
-
-      for (const subcategory of subcategories) {
-        wrapper.append(createCategoryButton(subcategory));
-      }
-    }
-  });
 
   return wrapper;
 }
