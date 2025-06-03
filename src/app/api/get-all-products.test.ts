f5e9379859db748ac6f50ec5b5fe07ffeb3714dd
Tanya <tarpel.gta@gmail.com>
2025-06-03 03:05:18 +0300

    refactor: 155-getAllProducts test

--- src/app/api/get-all-products.test.ts
+++ src/app/api/get-all-products.test.ts
@@ -1,10 +1,29 @@
-import { describe, expect, it, vi } from 'vitest';
+import { describe, beforeEach, expect, it, vi } from 'vitest';
 
 import { getAllProducts } from './get-all-products';
 import { isFetchError } from '../../utils/type-guards/is-fetch-error';
+import * as authService from '../auth-service';
+
+const MOCK_TOKEN = 'mock-token';
 
 describe('test getAllProducts function', () => {
+  beforeEach(() => {
+    vi.resetAllMocks();
+    vi.spyOn(authService, 'getToken').mockReturnValue(MOCK_TOKEN);
+  });
+
   it('return products with valid data', async () => {
+    globalThis.fetch = vi.fn().mockResolvedValue({
+      ok: true,
+      json: async () => ({
+        count: 1,
+        limit: 50,
+        offset: 0,
+        total: 1,
+        results: [],
+      }),
+    });
+
     const products = await getAllProducts();
 
     if (isFetchError(products)) {
