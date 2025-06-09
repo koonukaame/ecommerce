c4873d68734709bf9dfe9cabeae6dd5431b46f3d
Tanya <tarpel.gta@gmail.com>
2025-06-10 00:43:45 +0300

    feat: 159-create add-to-cart-button component

---
+++ src/shared/components/add-to-cart-button.ts
@@ -0,0 +1,48 @@
+import { createButton } from '../../utils/create-elements/create-tags';
+import { addProductToCart } from '../../app/api/add-product-to-cart';
+import { BUTTON } from '../styles';
+import { getOrCreateCart } from '../../app/api/get-or-create-cart';
+import { isFetchError } from '../../utils/type-guards/is-fetch-error';
+import { createPopupMessage } from './popup';
+
+export function addProductButton(attributes: Record<string, string>): HTMLButtonElement {
+  return createButton({
+    classes: [...BUTTON.general, ...BUTTON.generalHover, ...BUTTON.generalFocus, ...BUTTON.generalDisabled],
+    attributes: attributes,
+    events: {
+      click: (event: Event) => {
+        if (event.target instanceof HTMLButtonElement) {
+          addButtonClick(event.target);
+        }
+      },
+    },
+    text: 'Add to Cart',
+  });
+}
+
+const MESSAGES = {
+  success: 'Product added in cart successfully',
+  error: 'Can`t add product in cart',
+};
+
+async function addButtonClick(addButton: HTMLButtonElement): Promise<void> {
+  const cart = await getOrCreateCart();
+
+  if (isFetchError(cart)) {
+    console.log(`cart error: ${cart.message}`);
+    return;
+  }
+
+  if (addButton.dataset.id) {
+    const updatedCart = await addProductToCart(cart, addButton.dataset.id);
+
+    if (isFetchError(updatedCart)) {
+      createPopupMessage(MESSAGES.error, false);
+      return;
+    } else {
+      createPopupMessage(MESSAGES.success, true);
+    }
+  }
+
+  addButton.disabled = true;
+}
