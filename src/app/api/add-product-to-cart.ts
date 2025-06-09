7347e66a6fb16ba85d9501542bce37db5ccf7cd6
Alex <strelchenko.aleksandr@yandex.ru>
2025-06-09 13:56:06 +0300

    feat: 161-create addProductToCart function

---
+++ src/app/api/add-product-to-cart.ts
@@ -0,0 +1,38 @@
+import { API_URL, PROJECT_KEY } from '../constants';
+import { type FetchError } from '../types';
+import type { Cart } from '@commercetools/platform-sdk';
+
+export async function addProductToCart(
+  token: string,
+  cartId: string,
+  cartVersion: number,
+  productId: string,
+): Promise<Cart | FetchError> {
+  const response = await fetch(`${API_URL}/${PROJECT_KEY}/me/carts/${cartId}`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      version: cartVersion,
+      actions: [
+        {
+          action: 'addLineItem',
+          productId,
+        },
+      ],
+    }),
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+
+    return { message: error.message || 'Failed to add the product to cart' };
+  }
+
+  const updatedCart = await response.json();
+  console.log('Товар добавлен в корзину', updatedCart);
+
+  return updatedCart;
+}
