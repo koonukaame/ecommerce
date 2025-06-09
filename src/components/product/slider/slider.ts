66051abe78a7e9bd24fe4c4a6838adf93098b880
Tanya <tarpel.gta@gmail.com>
2025-06-10 00:56:26 +0300

    fix: 159-slider

--- src/components/product/slider/slider.ts
+++ src/components/product/slider/slider.ts
@@ -13,44 +13,45 @@ import { SLIDER_CLASSES } from '../../../pages/product/constants';
 
 Swiper.use([EffectFade, Navigation, Pagination]);
 
+const containerSlider = createDiv({ classes: SLIDER_CLASSES.containerSlider });
+const container = createDiv({ classes: ['swiper-container'], parent: containerSlider });
+
 export function ProductSlider(slides: Image[] | undefined): HTMLElement {
   if (!slides) {
     return createDiv({ classes: ['w-[50%]', 'px-5'], text: 'NO IMAGES' });
   }
 
-  const containerSlider = createDiv({ classes: SLIDER_CLASSES.containerSlider });
-  const container = createDiv({ classes: ['swiper-container'], parent: containerSlider });
+  container.textContent = '';
   const wrapper = createDiv({ classes: SLIDER_CLASSES.wrapper, parent: container });
-
   appendImages(wrapper, slides, SLIDER_CLASSES.imageContainer, SLIDER_CLASSES.image, {
     click: (event) => createModalSlider(event, slides),
   });
 
-  if (slides.length > 1) {
-    createDiv({ classes: ['swiper-button-prev'], parent: container });
-
-    createDiv({ classes: ['swiper-button-next'], parent: container });
-
-    createDiv({ classes: ['swiper-pagination'], parent: container });
-
-    setTimeout(() => {
-      new Swiper('.swiper-container', {
-        loop: true,
-        effect: 'fade',
-        fadeEffect: {
-          crossFade: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-      });
-    }, 0);
-  }
-
   return containerSlider;
+}
+
+export function initSlider(): void {
+  createDiv({ classes: ['swiper-button-prev'], parent: container });
+
+  createDiv({ classes: ['swiper-button-next'], parent: container });
+
+  createDiv({ classes: ['swiper-pagination'], parent: container });
+
+  setTimeout(() => {
+    new Swiper('.swiper-container', {
+      loop: true,
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }, 0);
 }
