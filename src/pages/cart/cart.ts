e3529db12230d7932967760d83f5ecd25f882d29
Alex <strelchenko.aleksandr@yandex.ru>
2025-06-12 23:22:10 +0300

    refactor: 179-refactor code

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,26 +1,9 @@
 import { cartLayout } from '../../components/cart/layout';
 import { container } from '../../shared/components/container';
-import { createButton } from '../../utils/create-elements/create-tags';
-import { applyPromocode } from '../../app/api';
 
 export function CartPage(): HTMLElement {
   const layout = cartLayout();
-  const button = createButton({});
-  button.style.marginRight = '20px';
-  button.style.padding = '5px';
-  button.style.cursor = 'pointer';
-  button.style.border = '2px solid black';
-  button.textContent = 'Кнопка';
+  container.append(layout);
 
-  button.addEventListener('click', async () => {
-    const priseWithPromoCode = await applyPromocode('WELCOME15');
-
-    console.log(
-      'Обновленная корзина с 15% скидкой на все товары в "totalPrice" свойстве объекта корзины',
-      priseWithPromoCode,
-    );
-  });
-
-  container.append(layout, button);
   return container;
 }
