e82201c934e59d49808c8855d23d1148902c64ee
Alex <strelchenko.aleksandr@yandex.ru>
2025-06-14 11:49:35 +0300

    feat: 184-implement shopping cart clearing functionality

--- src/shared/ui-config/button.ts
+++ src/shared/ui-config/button.ts
@@ -16,7 +16,10 @@ import { loginAndSaveToken } from '../../app/auth-service';
 import { removeFromCart } from '../../utils/cart-actions/remove-from-cats';
 import { addToCart } from '../../utils/cart-actions/add-to-cart';
 
-type Button = Record<'login' | 'main' | 'registration' | 'reset' | 'addToCart' | 'removeFromCart', ButtonProps>;
+type Button = Record<
+  'login' | 'main' | 'registration' | 'reset' | 'addToCart' | 'removeFromCart' | 'clearCart',
+  ButtonProps
+>;
 type ButtonProps = Omit<Options<'button'>, 'children' | 'parent' | 'tag'>;
 
 const BUTTON_CLASSES = [...BUTTON.general, ...BUTTON.generalHover, ...BUTTON.generalFocus];
@@ -141,5 +144,16 @@ export const BUTTONS_CONFIG: Button = {
       },
     },
     text: 'Remove from Cart',
+  },
+  clearCart: {
+    classes: [...BUTTON.general, ...BUTTON.generalHover, ...BUTTON.generalFocus, 'mt-[15px]'],
+    events: {
+      click: (event: Event) => {
+        if (event.target instanceof HTMLButtonElement) {
+          removeFromCart(event.target);
+        }
+      },
+    },
+    text: 'Clear cart',
   },
 };
--- src/utils/render-cart/render-cart.ts
+++ src/utils/render-cart/render-cart.ts
@@ -1,10 +1,13 @@
 import { getOrCreateCart } from '../../app/api/get-or-create-cart';
 import { createInfoMessage } from '../../shared/components/info-message';
-import { createA } from '../create-elements/create-tags';
+import { createA, createButton } from '../create-elements/create-tags';
 import { LINK_CONFIG } from '../../shared/ui-config/link';
 import { createCartItem } from '../../components/cart/item/item';
 import { isFetchError } from '../type-guards/is-fetch-error';
 import { CART_MESSAGES } from '../../pages/cart/constants';
+import { createPopupMessage } from '../../shared/components/popup';
+import { clearCart } from '../../app/api';
+import { BUTTONS_CONFIG } from '../../shared/ui-config/button';
 
 export async function renderCartItems(itemsWrapper: HTMLDivElement): Promise<void> {
   itemsWrapper.replaceChildren();
@@ -22,6 +25,31 @@ export async function renderCartItems(itemsWrapper: HTMLDivElement): Promise<voi
   } else {
     for (const item of cart.lineItems) {
       createCartItem(item, itemsWrapper);
+    }
+
+    const button = createButton(BUTTONS_CONFIG.clearCart);
+    button.style.alignSelf = 'flex-end';
+
+    itemsWrapper.append(button);
+
+    button.addEventListener('click', async () => {
+      clean();
+    });
+
+    async function clean(): Promise<void> {
+      const cart = await getOrCreateCart();
+
+      if (!('id' in cart)) {
+        createPopupMessage('Failed to clear the cart', false);
+        return;
+      }
+      const cleanedCart = await clearCart(cart);
+      console.log(cleanedCart);
+      itemsWrapper.replaceChildren();
+
+      createInfoMessage(itemsWrapper, CART_MESSAGES.emptyCart);
+      const catalogLink = createA(LINK_CONFIG.catalog);
+      itemsWrapper.append(catalogLink);
     }
   }
 }
