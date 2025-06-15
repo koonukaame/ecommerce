259757da3f69b70f35a22a894d308f4c7454a225
Tanya <tarpel.gta@gmail.com>
2025-06-15 13:50:16 +0300

    style: 182-remove console.log

--- src/utils/render-cart/render-cart.ts
+++ src/utils/render-cart/render-cart.ts
@@ -6,7 +6,6 @@ import { createCartItem } from '../../components/cart/item/item';
 import { isFetchError } from '../type-guards/is-fetch-error';
 import { CART_MESSAGES } from '../../pages/cart/constants';
 import { costEventEmitter } from '../../components/cart/item/total-cost';
-//import { totalCostComponent } from '../../components/cart/item/total-cost';
 
 export async function renderCartItems(itemsWrapper: HTMLDivElement): Promise<void> {
   itemsWrapper.replaceChildren();
